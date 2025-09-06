ee35157e699ce326897814f5014f365e258902a1
taskpul <romtietz@gmail.com>
2025-09-06 21:55:08 +0200

    feat: enforce tenant access on vendor and store routes

--- backend/apps/backend/src/api/middlewares.ts
+++ backend/apps/backend/src/api/middlewares.ts
@@ -5,12 +5,20 @@ import { hooksMiddlewares } from './hooks/middlewares'
 import { storeMiddlewares } from './store/middlewares'
 import { vendorMiddlewares } from './vendor/middlewares'
 import { resolveTenant } from '../shared/infra/http/middlewares/resolve-tenant'
+import {
+  filterByTenantId,
+  ensureTenantAccess,
+} from '../shared/infra/http/middlewares'
 
 export default defineMiddlewares({
   routes: [
     {
-      matcher: /.*/,
-      middlewares: [resolveTenant]
+      matcher: '/vendor*',
+      middlewares: [resolveTenant, ensureTenantAccess(), filterByTenantId()],
+    },
+    {
+      matcher: '/store*',
+      middlewares: [resolveTenant, ensureTenantAccess(), filterByTenantId()],
     },
     ...vendorMiddlewares,
     ...storeMiddlewares,
