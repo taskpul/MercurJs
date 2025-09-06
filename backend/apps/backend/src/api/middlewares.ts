cd374a8a9dd6abcb265cdf006851be4d780f9cf6
taskpul <romtietz@gmail.com>
2025-09-06 17:04:36 +0200

    feat: add tenant routing support

--- backend/apps/backend/src/api/middlewares.ts
+++ backend/apps/backend/src/api/middlewares.ts
@@ -4,9 +4,14 @@ import { adminMiddlewares } from './admin/middlewares'
 import { hooksMiddlewares } from './hooks/middlewares'
 import { storeMiddlewares } from './store/middlewares'
 import { vendorMiddlewares } from './vendor/middlewares'
+import { resolveTenant } from '../shared/infra/http/middlewares/resolve-tenant'
 
 export default defineMiddlewares({
   routes: [
+    {
+      matcher: /.*/,
+      middlewares: [resolveTenant]
+    },
     ...vendorMiddlewares,
     ...storeMiddlewares,
     ...adminMiddlewares,
---
+++ storefront/src/app/[tenant]/[locale]/(main)/page.tsx
@@ -0,0 +1,1 @@
+export { default, generateMetadata } from '../../../../[locale]/(main)/page'
